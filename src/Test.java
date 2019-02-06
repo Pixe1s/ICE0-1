fc9584ba4a18b2d2c2905ad86b6a62fee05c314d
Pixe1s <46787050+Pixe1s@users.noreply.github.com>
2019-02-05 22:40:08 -0500

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,12 @@
 // Two errors, Rule 10
 // https://wiki.sei.cmu.edu/confluence/pages/viewpage.action?pageId=88487735
 public class Test{
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
         Example example = new Example();
         Thread thread = new Thread(example);
-        thread.run();
-        thread.stop();
+        thread.start();
+        Thread.sleep(5000);
+        thread.interrupt();
     }
 
     public static class Example implements Runnable{
