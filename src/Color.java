b429fb0f79e61e14cc2528640f2387ed8b634382
Pixe1s <46787050+Pixe1s@users.noreply.github.com>
2019-02-05 22:36:36 -0500

    Update Color.java

--- src/Color.java
+++ src/Color.java
@@ -1,15 +1,9 @@
 //One error Rule 1
 // https://wiki.sei.cmu.edu/confluence/pages/viewpage.action?pageId=88487858
 public class Color {
-    int r;
-    int g;
-    int b;
-
-    public Color() {
-        r = 245;
-        g = 123;
-        b = 70;
-    }
+    int r = 245;
+    int g = 123;
+    int b = 70;
 
     public Color(int r, int g, int b) {
         this.r = r;
